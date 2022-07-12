1431923fe9986e24e20bdff4152a615f5dfad9ec
Abdul Rahman <abdul.095722@gmail.com>
2022-07-12 14:49:20 +0500

    removed commented code

--- uploaderdjangoapi/settings.py
+++ uploaderdjangoapi/settings.py
@@ -93,9 +93,9 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10
 }
